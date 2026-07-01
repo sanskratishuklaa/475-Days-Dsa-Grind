b0ca10a4c3b2e96e2d1113e48a0d24b0305e3a4c
Sanskrati Shukla <193257292+sanskratishuklaa@users.noreply.github.com>
2026-07-01 23:46:48 +0530

    Time: 0 ms (100%), Space: 13.7 MB (42.85%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,20 +1,40 @@
 class Solution {
 public:
+
     int searchInsert(vector<int>& nums, int target) {
-        int start=0;
-        int end=nums.size()-1;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]==target){
+
+        // Starting index of the array
+        int start = 0;
+
+        // Last index of the array
+        int end = nums.size() - 1;
+
+        // Perform Binary Search until start crosses end
+        while (start <= end) {
+
+            // Find the middle index
+            // This formula avoids integer overflow
+            int mid = start + (end - start) / 2;
+
+            // If target is found, return its index
+            if (nums[mid] == target) {
                 return mid;
-            }  
-            if(nums[mid]>target){
-                end=mid-1;
-            } 
-            else{
-                start=mid+1;
-            }    
-             }
+            }
+
+            // If target is smaller than middle element,
+            // search in the left half
+            if (nums[mid] > target) {
+                end = mid - 1;
+            }
+
+            // Otherwise search in the right half
+            else {
+                start = mid + 1;
+            }
+        }
+
+        // If target is not found,
+        // 'start' will point to the correct insert position
         return start;
     }
 };
