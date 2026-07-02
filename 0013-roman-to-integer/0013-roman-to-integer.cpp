e325264b0937704166afeeab66f41747deea1a4b
Sanskrati Shukla <193257292+sanskratishuklaa@users.noreply.github.com>
2026-07-02 23:49:25 +0530

    Time: 1 ms (81.03%), Space: 10 MB (86.55%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,34 +1,64 @@
 class Solution {
 public:
-int num(char c){
-    if(c=='I')
-    return 1;
-    else if(c=='V')
-    return 5;
-    else if(c=='X')
-    return 10;
-    else if(c=='L')
-    return 50;
-    else if(c=='C')
-    return 100;
-    else if(c=='D')
-    return 500;
-    else 
-    return 1000;
-}
+
+    // Function to convert a Roman character into its integer value
+    int num(char c){
+
+        if(c == 'I')
+            return 1;
+
+        else if(c == 'V')
+            return 5;
+
+        else if(c == 'X')
+            return 10;
+
+        else if(c == 'L')
+            return 50;
+
+        else if(c == 'C')
+            return 100;
+
+        else if(c == 'D')
+            return 500;
+
+        else
+            return 1000;   // If none of the above, it must be 'M'
+    }
+
     int romanToInt(string s) {
-        int sum=0, index=0;
-        while(index<s.size()-1){
+
+        // Variable to store the final answer
+        int sum = 0;
+
+        // Start checking from the first character
+        int index = 0;
+
+        // Loop until the second last character
+        while(index < s.size() - 1){
+
+            // If current Roman value is smaller than next Roman value,
+            // subtract it because it forms a special Roman number.
+            // Example:
+            // IV = 5 - 1 = 4
+            // IX = 10 - 1 = 9
             if(num(s[index]) < num(s[index+1]))
-                sum = sum-num(s[index]);
-            
+                sum = sum - num(s[index]);
+
+            // Otherwise simply add its value.
+            // Example:
+            // VI = 5 + 1 = 6
             else
-                sum = sum+num(s[index]);
-            
+                sum = sum + num(s[index]);
+
+            // Move to next character
             index++;
         }
-        
-        sum = sum+num(s[index]);
+
+        // Add the last character because it has no next character
+        sum = sum + num(s[index]);
+
+        // Return the final integer
         return sum;
     }
 };
